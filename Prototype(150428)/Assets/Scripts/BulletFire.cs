643d96a0a2b1fd3b25c62ea7405f67ab61b1127a
jangseongwoo <berkiss231@naver.com>
2015-04-29 16:07:27 +0900

    Prototype
    150428

---
+++ Prototype(150428)/Assets/Scripts/BulletFire.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletFire : MonoBehaviour {
+    //총알의 생명 시간.
+    private float LIFETime = 2.0f;
+	void Start () {
+	    //총알을 Forward 방향으로 힘을 가함.
+        rigidbody.AddRelativeForce(Vector3.forward * 1500.0f);
+        //일정 시간 지난 후 제거.
+        Destroy(gameObject, LIFETime);
+	}
+}
---
+++ Prototype(150428)/Assets/Scripts/GuiManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GuiManager : MonoBehaviour {
+    public GameObject ovrCamera;
+    public GameObject mainCamera;
+
+    private float lastSecond;
+    private float nowSecond;
+
+    void Awake()
+    {
+        ovrCamera = GameObject.FindGameObjectWithTag("OVRCAMERA");
+        mainCamera = GameObject.FindGameObjectWithTag("MainCamera");
+    }
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect(20, 20, 100, 25), "Restart"))
+        {
+            lastSecond = Time.time;
+            Application.LoadLevel("jeongsoover");
+        }
+        //nowSecond = float.Parse(Time.time.ToString());
+        //nowSecond -= lastSecond;
+
+        nowSecond += Time.deltaTime;
+
+
+        GUI.Box(new Rect(20, 50, 200, 25), nowSecond.ToString());
+        //GUI.Box(new Rect(20, 50, 200, 25), Time.time.ToString());
+        
+    }
+
+}
